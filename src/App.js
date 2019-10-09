669b18bf4fcda4353b8293f4bd43cac6d3a24f66
Anna Presnyakova <apresnyakova88@gmail.com>
2019-10-09 15:55:06 +0200

    Navbar component, displaying one User: UserItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Navbar title='Test' />
+            <div className='container'>
+                <UserItem />
+            </div>
+        </div>
+    );
 }
 
 export default App;
