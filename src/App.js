8e7dc0f9754af4c5c24716f89b5a7bf445b02976
Anna <apresnyakova88@gmail.com>
2019-10-17 22:20:50 +0200

    fetch users from github, create Spinner component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
-function App() {
-    return (
-        <div className='App'>
-            <Navbar title='Test' />
-            <div className='container'>
-                <Users />
+class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    };
+    async componentDidMount() {
+        this.setState({ loading: true });
+        const res = await axios.get('https://api.github.com/users');
+        this.setState({ users: res.data, loading: false });
+    }
+    render() {
+        return (
+            <div className='App'>
+                <Navbar title='Test' />
+                <div className='container'>
+                    <Users loading={this.state.loading} users={this.state.users} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,38 +1,25 @@
 import React, { Component } from 'react';
-import UserItem from './UserItem'
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-export class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            },
-            {
-                id: '2',
-                login: 'mojombo2',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            }
-        ]
-    };
-    render() {
+const Users = ({ users, loading }) => {
+    if (loading) {
+        return <Spinner />;
+    } else {
         return (
             <div style={usersStyle}>
-                {this.state.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
             </div>
         );
     }
-}
+};
 
 const usersStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
-}
+};
 
 export default Users;
